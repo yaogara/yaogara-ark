e2bc8e5f1ddb9dffa40b57b507b17be7106cb52b
DaddyGringo <40545026+gringodaddy@users.noreply.github.com>
2025-10-27 13:02:51 -0500

    Update baseUrl in quartz.config.ts to remove the protocol, streamlining the configuration for deployment.

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://yaogara.github.io/yaogara-ark",
+    baseUrl: "yaogara.github.io/yaogara-ark",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {

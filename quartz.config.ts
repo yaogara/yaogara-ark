4e24711471014d957725199cf4cb0c953c789820
DaddyGringo <40545026+gringodaddy@users.noreply.github.com>
2025-10-27 21:05:19 -0500

    fix: update baseUrl in quartz.config.ts to ark.yaogara.org

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "yaogara.github.io/yaogara-ark",
+    baseUrl: "ark.yaogara.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {

fac4732a912f9c6a1d544b68a9c874d90e1e8635
yaogara <aya@yage.se>
2025-10-29 13:01:18 -0500

    Update quartz.config.ts
    Mermaid disable

--- quartz.config.ts
+++ quartz.config.ts
@@ -66,7 +66,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false, mermaid: false,}),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
